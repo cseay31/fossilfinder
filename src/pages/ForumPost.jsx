8a29714f74e96698fef4776a55f0db2eb91ec780
Connor Seay <seayc31@ensworth.com>
2026-03-02 22:05:44 +0000

    File changes

--- src/pages/ForumPost.jsx
+++ src/pages/ForumPost.jsx
@@ -11,8 +11,7 @@ import ReactMarkdown from 'react-markdown';
 import BackButton from '../components/mobile/BackButton';
 
 export default function ForumPostPage({ isDarkMode }) {
-  const urlParams = new URLSearchParams(window.location.search);
-  const id = urlParams.get('id');
+  const postId = new URLSearchParams(window.location.search).get('id');
   const [post, setPost] = useState(null);
   const [replies, setReplies] = useState([]);
   const [isLoadingReplies, setIsLoadingReplies] = useState(true);
